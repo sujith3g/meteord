6b0852ad548770fd73a85f08259d00ea8222c933
Sujith G <sujith3g@gmail.com>
2017-11-16 10:32:28 +0530

    Update Node to 8.9.1

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -3,11 +3,11 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-base" ../base && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-base" abernix/meteord:base
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-binbuild" abernix/meteord:binbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "sujith3g/meteord:node-${NODE_VERSION}-base" ../base && \
+  docker tag "sujith3g/meteord:node-${NODE_VERSION}-base" sujith3g/meteord:base
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "sujith3g/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
+  docker tag "sujith3g/meteord:node-${NODE_VERSION}-onbuild" sujith3g/meteord:onbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "sujith3g/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
+  docker tag "sujith3g/meteord:node-${NODE_VERSION}-devbuild" sujith3g/meteord:devbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "sujith3g/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
+  docker tag "sujith3g/meteord:node-${NODE_VERSION}-binbuild" sujith3g/meteord:binbuild
--- tests/test_phantomjs.sh
+++ tests/test_phantomjs.sh
@@ -12,7 +12,7 @@ clean
 docker run  \
     --name phantomjs_check \
     --entrypoint="/bin/bash" \
-    "abernix/meteord:node-${NODE_VERSION}-base" -c 'phantomjs -h'
+    "sujith3g/meteord:node-${NODE_VERSION}-base" -c 'phantomjs -h'
 
 sleep 5
 
